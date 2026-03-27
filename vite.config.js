6a98cd9abd7e39567a594d336a31db5d15b9c507
Tobias Estivalete <estivaletetobias@gmail.com>
2026-03-27 10:01:20 -0300

    Ajsutes Site

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,14 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
+        metodo: resolve(__dirname, 'metodo.html'),
+        emagrecimento: resolve(__dirname, 'emagrecimento.html'),
+        avaliacao: resolve(__dirname, 'avaliacao.html'),
+        servicos: resolve(__dirname, 'servicos.html'),
+        conteudos: resolve(__dirname, 'conteudos.html'),
+        ebooks: resolve(__dirname, 'ebooks.html'),
+        depoimentos: resolve(__dirname, 'depoimentos.html'),
+        contato: resolve(__dirname, 'contato.html'),
         florianopolis: resolve(__dirname, 'personal-trainer-florianopolis/index.html'),
       },
     },
